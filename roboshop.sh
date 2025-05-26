8a6001bbc5fee82429ab0777478426bdb814e967
mano jyoti <manojyoti434@gmail.com>
2025-05-26 20:54:03 +0530

    shell roboshop CLI

--- roboshop.sh
+++ roboshop.sh
@@ -28,7 +28,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$INSTANCES'.'$DOMAIN_NAME"
+            "Name"              : "'$INSTANCES'.'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
